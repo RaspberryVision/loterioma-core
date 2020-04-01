e1a3f113b903dec8cb5ea06e0c0e060c7c1fd28d
RafalMalik <rafalmalik.info@gmail.com>
2020-04-01 23:22:09 +0200

    Update Abstract Network definition.

--- src/NetworkHelper/AbstractNetworkHelper.php
+++ src/NetworkHelper/AbstractNetworkHelper.php
@@ -15,6 +15,7 @@ namespace App\NetworkHelper;
 
 use App\Model\DTO\Network\NetworkResponse;
 use App\Model\DTO\Network\NetworkRequestInterface;
+use App\Model\DTO\Network\NetworkResponseInterface;
 
 abstract class AbstractNetworkHelper
 {
@@ -65,45 +66,45 @@ abstract class AbstractNetworkHelper
     }
 
     /**
-     * Method call API request to NetworkComponent.
+     * Make network request to component.
+     * Perform CURL request for given parameters - supports all HTTP methods.
      *
-     * @param string $endpoint
-     * @param array $requestParams
-     * @return NetworkResponse
+     * @param NetworkRequestInterface $networkRequest
+     * @return NetworkResponseInterface
      */
-    protected function makeGetRequest(string $endpoint, array $requestParams): NetworkResponse
+    protected function makeRequest(NetworkRequestInterface $networkRequest): NetworkResponseInterface
     {
-        $curlResponse = $this->makeCurl(
-            $endpoint,
-            json_encode($requestParams)
-        );
+        $ch = curl_init();
 
-        return $this->createResponse($curlResponse);
-    }
+        curl_setopt($ch, CURLOPT_URL, $this->getRequestUrl($networkRequest->getEndpoint()));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $networkRequest->getMethod());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'LM-COMP-HASH: ' . $networkRequest->getComponentHash(),
+            'LM-TIME: ' . date('Y-m-d H:i:s'),
+            'LM-REQUEST-HASH: ' . uniqid('eng_rng_', true),
+            'Content-Type: application/ld+json'
+        ]);
 
-    /**
-     * @param string $endpoint
-     * @param string $parameters
-     * @return mixed
-     */
-    private function makeCurl(string $endpoint, string $parameters)
-    {
-        $curl = curl_init();
+        if ('POST' === $networkRequest->getMethod()) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+        }
 
-        curl_setopt_array(
-            $curl,
-            [
-                CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => $this->getRequestUrl($endpoint),
-                CURLOPT_POST => 1,
-                CURLOPT_POSTFIELDS => $parameters,
-            ]
-        );
+        if (count($networkRequest->getRequestParams()) > 0) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($networkRequest->getRequestParams()));
+        }
 
-        $jsonResponse = curl_exec($curl);
-        curl_close($curl);
+        $response = curl_exec($ch);
+        if (!$response) {
+            return $this->createResponse(json_encode([
+                'message' => curl_error($ch),
+                'code' => curl_errno($ch)
+            ]));
+        }
 
-        return $jsonResponse;
+        curl_close($ch);
+
+        return $this->createResponse($response);
     }
 
     /**
